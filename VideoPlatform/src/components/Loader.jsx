f487a44828b99ada6aaf052f1dc9d5c4d9f0c3b1
Cynthy Niganze <cynthynigaira@gmail.com>
2026-03-15 16:25:17 +0200

    styling the page

--- VideoPlatform/src/components/Loader.jsx
+++ VideoPlatform/src/components/Loader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Loader = () => {
-  return <div className="loader">Loading...</div>;
+  return <div className="loader text-center">Wait a bit...</div>;
 };
 
 export default Loader;
